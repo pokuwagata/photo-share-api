69ad55372136c62b38f7a0fb98244e045ed6982c
pokuwagata <pokuwagata@gmail.com>
2020-05-06 13:48:14 +0900

    commit

--- index.js
+++ index.js
@@ -1,49 +1,36 @@
-const { ApolloServer } = require(`apollo-server`);
+const { ApolloServer } = require(`apollo-server-express`);
+const express = require(`express`);
+const expressPlayground = require(`graphql-playground-middleware-express`)
+  .default;
+const { readFileSync } = require(`fs`);
+const typeDefs = readFileSync(`./typeDefs.graphql`, `UTF-8`);
+const resolvers = require(`./resolvers`);
 
-const typeDefs = `
-type Photo {
-  id: ID!
-  url: String!
-  name: String!
-  description: String
-}
-type Query {
-  totalPhotos: Int!
-  allPhotos: [Photo!]!
-}
-type Mutation {
-  postPhoto(name: String!, description: String): Photo
-}`
-;
+const { MongoClient } = require(`mongodb`);
+require(`dotenv`).config();
 
-var photos = [];
-var _id = 0;
-
-const resolvers = {
-  Query: {
-    totalPhotos: () => photos.length,
-    allPhotos: () => photos
-  },
-  Mutation: {
-    postPhoto(parent, args) {
-      // 2. 新しい写真を作成し、idを生成する
-      var newPhoto = {
-        id: _id++,
-        ...args
-      };
-      photos.push(newPhoto);
-      return newPhoto;
+async function start() {
+  var app = express();
+  const MONGO_DB = process.env.DB_HOST;
+  const client = await MongoClient.connect(MONGO_DB, { useNewUrlParser: true });
+  const db = client.db();
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: async ({ req }) => {
+      const githubToken = req.headers.authorization;
+      const currentUser = await db.collection("users").findOne({ githubToken });
+      return { db, currentUser };
     }
-  },
-  Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
-  }
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
-  // Webサーバーを起動 server
-  .listen()
-  .then(({ url }) => console.log(`GraphQL Service running on ${url}`));
+  });
+  server.applyMiddleware({ app });
+  app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShare API`));
+  app.get(`/playground`, expressPlayground({ endpoint: `/graphql` }));
+  app.listen({ port: 4000 }, () =>
+    console.log(
+      `GraphQL Server running at http://localhost:4000${server.graphqlPath}`
+    )
+  );
+}
+// 5. start関数を実行 start()
+start();
--- note.md
+++ note.md
@@ -1,4 +1,14 @@
 # note.md
 
-- queryとmutationは同時にできない？
+- queryと`mutation`は同時にできない？
 - リテラルのgraphqlに対して補完やフォーマットは効かない？
+- MongoDB接続失敗の解決
+  - <https://stackoverflow.com/questions/55499175/how-to-fix-error-querysrv-erefused-when-connecting-to-mongodb-atlas>
+- graphqlでは文字列リテラルは"のみ
+- localhost:3000はあえてアクセスできない場所を指定しているっぽい
+- Playgroundからdebugするときどうやる？
+- ctrl+cするとアプリが残ることがある
+  - `lsof -i tcp:4000` , `kill -9 pid` で解決
+- currentuserが動作しない
+  - node-fetchがインストールされていない？→変わらない
+  - 
